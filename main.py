97dbcd388c7490d6ba845263daf72f262a14b93e
Zastial <sasandre10@gmail.com>
2022-07-08 01:56:23 +0200

    Adding a bad moves counter

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def open_the_list(fich):
     return lines
 
 
-def game(namefich):
+def game(namefich, maxHitCount):
     liste_mots = open_the_list(namefich) # Create the list with the words
 
     motRand = liste_mots[random.randint(0,len(liste_mots)-1)]
@@ -19,6 +19,9 @@ def game(namefich):
 
     score = ["_ " for i in range(len(motRand))]
     print("".join(score)) # Create the score which will be the correct letters entered
+
+    hitCount = 0
+    print(f'number of max moves : {maxHitCount} | number of bad moves made : {hitCount}')
 
     notfind = True
     while(notfind): # While every letter as not been found, the game continue
@@ -32,8 +35,16 @@ def game(namefich):
         for i in range(len(motRand)):
             if(ask == motRand[i]): # If the letter entered is in the word chose a the start, it'll be displayed
                 score[i] = motRand[i]
+        
+        if ask not in motRand:
+            hitCount += 1
 
         print("".join(score)) # Display the score each time the player enters a letter
+        print(f'number of max moves : {maxHitCount} | number of bad moves made : {hitCount}')
+
+        if hitCount == maxHitCount:
+            print("Sorry you loose !")
+            break
 
         cpt = 0
         for v in score: #Check if the game is finished
@@ -42,7 +53,8 @@ def game(namefich):
         if cpt == len(motRand):
             notfind = False
 
-    print("Good job ! You found the word !!")
+    if notfind == False:
+        print("Good job ! You found the word !!")
 
 
-game("liste_francais")
+game("liste_francais",5)
